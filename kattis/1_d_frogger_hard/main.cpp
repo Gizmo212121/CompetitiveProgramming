3ef7a0664b15649db0b067e1f5869800d6bf971a
= <ljrhhb@mst.edu>
2024-08-30 21:42:19 -0500

    I hate this problem

--- kattis/1_d_frogger_hard/main.cpp
+++ kattis/1_d_frogger_hard/main.cpp
@@ -1,121 +1,332 @@
-#include <cstdlib>
-#include <iostream>
 #include <vector>
+#include <iostream>
+#include <random>
+#include <unordered_set>
+#include <chrono>
 
-int runIteration(const int startingFrogIndex, const std::vector<int>& board, const int numberOfBoardSquares, std::vector<int>& repeatedIndices, const int specialRepeatedIndexValue, std::vector<int>& previousTotalMagicNumbers, std::vector<int>& cycleIndices)
+// #define MAX_BOARD_SIZE 200'000
+#define MAX_BOARD_SIZE 1000'000
+
+void runIteration(const int startingIndex, int board[], const int numberOfBoardSquares, std::vector<int>& repeatedIndices, std::vector<int>& previousTotalMagicNumbers, std::vector<std::vector<int>>& subpathDistinctMagicNumbers, int& olist, int& oloop, int& odistinct)
 {
-    if (previousTotalMagicNumbers[startingFrogIndex - 1] != -1)
+    olist++;
+
+    if (subpathDistinctMagicNumbers[startingIndex].size() > 0)
     {
-        return 0;
+        return;
     }
 
     int totalMagicNumbersFound = 0;
 
-    int frogIndex = startingFrogIndex;
+    std::vector<int> distinctMagicNumbers;
+    std::unordered_set<int> distinctMagicNumbersMap;
+    // distinctMagicNumbers.
+
+
+    int currentIndex = startingIndex;
 
     std::vector<int> previouslyFoundPieceIndices;
-    previouslyFoundPieceIndices.resize(0);
+    previouslyFoundPieceIndices.reserve(numberOfBoardSquares);
 
     while (true)
     {
-        if (cycleIndices[frogIndex - 1] != -1)
+        oloop++;
+
+        previouslyFoundPieceIndices.push_back(currentIndex);
+        repeatedIndices[currentIndex] = startingIndex;
+
+        if (distinctMagicNumbersMap.find(board[currentIndex]) == distinctMagicNumbersMap.end())
         {
-            int cycleMagicNumbers = cycleIndices[frogIndex - 1];
-
-            for (int i = 0; i < totalMagicNumbersFound; i++)
-            {
-                previousTotalMagicNumbers[previouslyFoundPieceIndices[i]] = totalMagicNumbersFound + cycleMagicNumbers - i;
-            }
-
-            return totalMagicNumbersFound;
+            distinctMagicNumbersMap.insert(board[currentIndex]);
         }
-        else if (previousTotalMagicNumbers[frogIndex - 1] != -1)
+
+        int numberAtFrogIndex = board[currentIndex];
+        currentIndex += numberAtFrogIndex;
+
+        if (currentIndex < 0 || currentIndex >= numberOfBoardSquares) { break; }
+
+        if (!subpathDistinctMagicNumbers[currentIndex].empty())
         {
-            totalMagicNumbersFound += previousTotalMagicNumbers[frogIndex - 1];
+            distinctMagicNumbers = subpathDistinctMagicNumbers[currentIndex];
+            // distinctMagicNumbersMap.insert(subpathDistinctMagicNumbers[currentIndex].begin(), subpathDistinctMagicNumbers[currentIndex].end());
 
             break;
         }
 
-        totalMagicNumbersFound++;
-
-        previouslyFoundPieceIndices.push_back(frogIndex - 1);
-
-        int numberAtFrogIndex = board[frogIndex - 1];
-
-        repeatedIndices[frogIndex - 1] = specialRepeatedIndexValue;
-
-        frogIndex += numberAtFrogIndex;
-
-        if (frogIndex <= 0) { break; }
-        else if (frogIndex > numberOfBoardSquares) { break; }
-
-        if (repeatedIndices[frogIndex - 1] == specialRepeatedIndexValue)
+        if (repeatedIndices[currentIndex] == startingIndex)
         {
+            // std::cout << "Found loop!\n";
             // Find the index value of the square that started the loop
 
             int indexOfStartOfLoopStart = -1;
             for (size_t i = 0; i < previouslyFoundPieceIndices.size(); i++)
             {
-                if (indexOfStartOfLoopStart == -1 && previouslyFoundPieceIndices[i] == frogIndex - 1)
+                if (indexOfStartOfLoopStart == -1 && previouslyFoundPieceIndices[i] == currentIndex)
                 {
-                    indexOfStartOfLoopStart = previouslyFoundPieceIndices[i];
+                    indexOfStartOfLoopStart = i;
                 }
 
                 if (indexOfStartOfLoopStart != -1)
                 {
-                    previousTotalMagicNumbers[previouslyFoundPieceIndices[i]] = totalMagicNumbersFound - indexOfStartOfLoopStart;
-                    cycleIndices[previouslyFoundPieceIndices[i]] = totalMagicNumbersFound - indexOfStartOfLoopStart;
-                }
-                else
-                {
-                    previousTotalMagicNumbers[previouslyFoundPieceIndices[i]] = totalMagicNumbersFound - i;
-                    cycleIndices[previouslyFoundPieceIndices[i]] = totalMagicNumbersFound - i;
-                }
+                    bool currentValueIsDistinct = true;
+                    for (size_t j = 0; j < distinctMagicNumbers.size(); j++)
+                    {
+                        odistinct++;
 
+                        if (distinctMagicNumbers[j] == board[previouslyFoundPieceIndices[i]])
+                        {
+                            currentValueIsDistinct = false;
+                            break;
+                        }
+                    }
+
+                    if (currentValueIsDistinct)
+                    {
+                        // std::cout << "Adding a distinct value: " << board[previouslyFoundPieceIndices[i]] << '\n';
+                        distinctMagicNumbers.push_back(board[previouslyFoundPieceIndices[i]]);
+                    }
+                }
             }
 
-            return totalMagicNumbersFound;
+            // std::cout << "Distinct magic numbers size: " << distinctMagicNumbers.size() << std::endl;
+
+            for (int i = indexOfStartOfLoopStart; i < previouslyFoundPieceIndices.size(); i++)
+            {
+                subpathDistinctMagicNumbers[previouslyFoundPieceIndices[i]] = distinctMagicNumbers;
+            }
+
+            for (int i = indexOfStartOfLoopStart - 1; i >= 0; i--)
+            {
+                bool currentValueIsDistinct = true;
+                for (size_t j = 0; j < distinctMagicNumbers.size(); j++)
+                {
+                    odistinct++;
+
+                    if (distinctMagicNumbers[j] == board[previouslyFoundPieceIndices[i]])
+                    {
+                        currentValueIsDistinct = false;
+                        break;
+                    }
+                }
+
+                if (currentValueIsDistinct)
+                {
+                    distinctMagicNumbers.push_back(board[previouslyFoundPieceIndices[i]]);
+                }
+
+                // std::cout << "DEBUG\n";
+                // std::cout << i << '\n';
+                // std::cout << previouslyFoundPieceIndices[i] << '\n';
+                subpathDistinctMagicNumbers[previouslyFoundPieceIndices[i]] = distinctMagicNumbers;
+            }
+
+            return;
         }
     }
 
-    for (int i = 0; i < totalMagicNumbersFound; i++)
+    for (int i = static_cast<int>(previouslyFoundPieceIndices.size()) - 1; i >= 0; i--)
     {
-        previousTotalMagicNumbers[previouslyFoundPieceIndices[i]] = totalMagicNumbersFound - i;
+        bool currentValueIsDistinct = true;
+        for (size_t j = 0; j < distinctMagicNumbers.size(); j++)
+        {
+            odistinct++;
+
+            if (distinctMagicNumbers[j] == board[previouslyFoundPieceIndices[i]])
+            {
+                currentValueIsDistinct = false;
+                break;
+            }
+        }
+
+        // bool currentValueIsDistinct = (distinctMagicNumbersMap.find(board[previouslyFoundPieceIndices[i]]) == distinctMagicNumbersMap.end());
+
+        if (currentValueIsDistinct)
+        {
+            distinctMagicNumbers.push_back(board[previouslyFoundPieceIndices[i]]);
+        }
+
+        subpathDistinctMagicNumbers[previouslyFoundPieceIndices[i]] = distinctMagicNumbers;
     }
 
-    return totalMagicNumbersFound;
+    return;
 }
+
+// void addDistinctSubpaths(const int start, const int end)
+// {
+//     for (int i = static_cast<int>(previouslyFoundPieceIndices.size()) - 1; i >= 0; i--)
+//     {
+//         bool currentValueIsDistinct = true;
+//         for (size_t j = 0; j < distinctMagicNumbers.size(); j++)
+//         {
+//             odistinct++;
+//
+//             if (distinctMagicNumbers[j] == board[previouslyFoundPieceIndices[i]])
+//             {
+//                 currentValueIsDistinct = false;
+//                 break;
+//             }
+//         }
+//
+//         // bool currentValueIsDistinct = (distinctMagicNumbersMap.find(board[previouslyFoundPieceIndices[i]]) == distinctMagicNumbersMap.end());
+//
+//         if (currentValueIsDistinct)
+//         {
+//             distinctMagicNumbers.push_back(board[previouslyFoundPieceIndices[i]]);
+//         }
+//
+//         subpathDistinctMagicNumbers[previouslyFoundPieceIndices[i]] = distinctMagicNumbers;
+//     }
+// }
+
+void generatePermutations(int depth, int numberOfBoardSquares, int board[], const int minVal, const int maxVal)
+{
+    if (depth == numberOfBoardSquares)
+    {
+        // Process the board permutation here (runIteration, etc.)
+        std::vector<int> repeatedIndices(numberOfBoardSquares, -1);
+        std::vector<std::vector<int>> subpathDistinctMagicNumbers(numberOfBoardSquares);
+        std::vector<int> previousTotalMagicNumbers(numberOfBoardSquares, -1);
+
+        int olist = 0;
+        int oloop = 0;
+        int odistinct = 0;
+
+        for (int i = 0; i < numberOfBoardSquares; i++)
+        {
+            runIteration(i, board, numberOfBoardSquares, repeatedIndices, previousTotalMagicNumbers, subpathDistinctMagicNumbers, olist, oloop, odistinct);
+        }
+
+        int sum = 0;
+
+        for (int i = 0; i < numberOfBoardSquares; i++)
+        {
+            sum += subpathDistinctMagicNumbers[i].size();
+        }
+
+        return;
+    }
+
+    for (int i = minVal; i <= maxVal; ++i)
+    {
+        board[depth] = i;
+        generatePermutations(depth + 1, numberOfBoardSquares, board, minVal, maxVal);
+    }
+}
+
+class Timer
+{
+
+public:
+
+    Timer(const std::string& functionName)
+    {
+        start = std::chrono::high_resolution_clock::now();
+        m_name = functionName;
+    }
+
+    ~Timer()
+    {
+        end = std::chrono::high_resolution_clock::now();
+
+        duration = end - start;
+
+        m_timerMessages.push_back(m_name + " elapsed time: " + std::to_string(duration.count()) + "s");
+    }
+
+    static void print()
+    {
+        for (const std::string& message : m_timerMessages)
+        {
+            std::cout << message << '\n';
+        }
+    }
+
+private:
+
+    std::string m_name;
+
+    std::chrono::time_point<std::chrono::system_clock> start;
+    std::chrono::time_point<std::chrono::system_clock> end;
+
+    std::chrono::duration<double> duration;
+
+    static std::vector<std::string> m_timerMessages;
+
+};
+
+std::vector<std::string> Timer::m_timerMessages;
 
 int main()
 {
-    int numberOfBoardSquares;
-    std::cin >> numberOfBoardSquares;
-    std::cin.ignore(1, '\n');
+    // int numberOfBoardSquares;
+    // std::cin >> numberOfBoardSquares;
+    // std::cin.ignore(1, '\n');
+    //
+    // int board[MAX_BOARD_SIZE];
+    //
+    // for (int i = 0; i < numberOfBoardSquares; i++)
+    // {
+    //     std::cin >> board[i];
+    // }
 
-    std::vector<int> board;
-    board.resize(numberOfBoardSquares);
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dist(-2000, 2000);
 
-    for (int i = 0; i < numberOfBoardSquares; i++)
+    const int numberOfBoardSquares = 1000000;
+    int board[MAX_BOARD_SIZE];
+    for (size_t i = 0; i < numberOfBoardSquares; i++) { board[i] = dist(gen); }
+
+    // const int numberOfBoardSquares = 6;  // Board size
+    // const int minVal = -5;               // Minimum value
+    // const int maxVal = 5;                // Maximum value
+    //
+    // std::vector<int> board(numberOfBoardSquares);
+    // generatePermutations(0, numberOfBoardSquares, board, minVal, maxVal);
+
+
+
     {
-        std::cin >> board[i];
+        Timer timer("main");
+
+        std::vector<int> repeatedIndices(numberOfBoardSquares, -1);
+        std::vector<std::vector<int>> subpathDistinctMagicNumbers(numberOfBoardSquares);
+        std::vector<int> previousTotalMagicNumbers(numberOfBoardSquares, -1);
+
+        int olist = 0;
+        int oloop = 0;
+        int odistinct = 0;
+
+
+        for (int i = 0; i < numberOfBoardSquares; i++)
+        {
+            runIteration(i, board, numberOfBoardSquares, repeatedIndices, previousTotalMagicNumbers, subpathDistinctMagicNumbers, olist, oloop, odistinct);
+        }
+
+        size_t sum = 0;
+
+        for (int i = 0; i < numberOfBoardSquares; i++) { sum += subpathDistinctMagicNumbers[i].size(); }
+
+        std::cout << sum << '\n';
+        // std::cout << "N: " << numberOfBoardSquares << '\n';
+        // std::cout << "olist: " << olist << '\n';
+        // std::cout << "oloop: " << oloop << '\n';
+        // std::cout << "odistinct: " << odistinct << '\n';
     }
 
-    std::vector<int> repeatedIndices(numberOfBoardSquares, -1);
-    std::vector<int> previousTotalMagicNumbers(numberOfBoardSquares, -1);
-    std::vector<int> cycleIndices(numberOfBoardSquares, -1);
+    Timer::print();
 
-    for (int i = 0; i < numberOfBoardSquares; i++)
-    {
-        runIteration(i + 1, board, numberOfBoardSquares, repeatedIndices, i + 1, previousTotalMagicNumbers, cycleIndices);
-    }
-
-    int sum = 0;
-    for (int i = 0; i < numberOfBoardSquares; i++)
-    {
-        sum += previousTotalMagicNumbers[i];
-    }
-
-    std::cout << sum << '\n';
+    // for (int i = 0; i < numberOfBoardSquares; i++)
+    // {
+    //     sum += subpathDistinctMagicNumbers[i].size();
+    //
+    //     // for (size_t j = 0; j < subpathDistinctMagicNumbers[i].size(); j++)
+    //     // {
+    //     //     std::cout << subpathDistinctMagicNumbers[i][j] << ", ";
+    //     // }
+    //     //
+    //     // std::cout << std::endl;
+    // }
 
     return 0;
 }
